cd1f206aa95aecdaded93f903f273f0ca9c1a883
kauane05 <126490321+kauane05@users.noreply.github.com>
2023-04-18 23:55:47 -0300

    envio da tarefa

---
+++ WpfAppCalculadoraPds/MainWindow.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppCalculadoraPds
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btMais_Click(object sender, RoutedEventArgs e)
+        {
+            Double a = Convert.ToDouble(tbValorA.Text);
+            Double b = Convert.ToDouble(tbValorB.Text);
+            Double t = a + b;
+            tbResultado.Text = a.ToString() + "+" + b.ToString() + "=" + t.ToString();
+        }
+
+        private void btMenos_Click(object sender, RoutedEventArgs e)
+        {
+            Double a = Convert.ToDouble(tbValorA.Text);
+            Double b = Convert.ToDouble(tbValorB.Text);
+            Double t = a - b;
+            tbResultado.Text = a.ToString() + "-" + b.ToString() + "=" + t.ToString();
+        }
+
+        private void btDivisao_Click(object sender, RoutedEventArgs e)
+        {
+            Double a = Convert.ToDouble(tbValorA.Text);
+            Double b = Convert.ToDouble(tbValorB.Text);
+            Double t = a / b;
+            tbResultado.Text = a.ToString() + "/" + b.ToString() + "=" + t.ToString();
+        }
+
+        private void btMultiplicacao_Click(object sender, RoutedEventArgs e)
+        {
+            Double a = Convert.ToDouble(tbValorA.Text);
+            Double b = Convert.ToDouble(tbValorB.Text);
+            Double t = a * b;
+            tbResultado.Text = a.ToString() + "*" + b.ToString() + "=" + t.ToString();
+        }
+    }
+}
